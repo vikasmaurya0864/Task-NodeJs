8b9985ca7006ff94385c42fd8b8fca2dcb9628dc
Vikas Maurya <86638724+vikasmaurya0864@users.noreply.github.com>
2022-12-06 10:06:34 +0530

    Create index.js

---
+++ async-await/index.js
@@ -0,0 +1,14 @@
+// async function db(){
+//         console.log("Entering in promise");
+//         let p1 = new Promise((resolve, reject)=>{
+//                 console.log("inside promise");
+//                 setTimeout(()=>{
+//                         resolve("Done!!!");
+//                 },2000)
+//                 console.log("leaving promise");
+//         })
+//         let result = await p1;
+//         console.log("Awaiting result");
+//         console.log(result);
+// }
+// db();
