8b3425f766ad34379193e1172a94fb88d733bf81
Vikas Maurya <86638724+vikasmaurya0864@users.noreply.github.com>
2022-12-10 15:58:16 +0530

    Create index.js

---
+++ functions/index.js
@@ -0,0 +1,14 @@
+// function sum(a,b){               //named function
+//    return a + b;
+// }
+// console.log(sum(5,5));
+
+// let multiply = (x,y)=>{              //anomynous function
+//     return x * y;
+// }
+// console.log(multiply(5,5));
+
+// (function add(c,d){                 //immediately invoked functions(IIFS)
+//     let p = c + d;
+//     console.log(p);
+// }(10,2))
